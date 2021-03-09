9cec3d50a2e38d0a5ea73303d6a75d21f0ead6e1
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-09 08:41:32 -0600

    Create initial project files from Spring initializer

---
+++ src/main/java/com/jlu/deployme/DeploymeApplication.java
@@ -0,0 +1,13 @@
+package com.jlu.deployme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlu/deployme/DeploymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlu.deployme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
